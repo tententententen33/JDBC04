6115b8b3f510d53a2cbecc6344fa3c0675ea8193
ten <bsi25017@uhe.ac.jp>
2026-04-17 15:02:44 +0900

    init

--- Jdbc04WhereSafe.java
+++ Jdbc04WhereSafe.java
@@ -10,19 +10,40 @@ import config.ConnectionFactory;
 
 public class Jdbc04WhereSafe {
 
+//	public static void main(String[] args) {
+//        int targetId = 111;
+//        String sql =
+//        		"SELECT LAST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID = ?";
+//
+//        try (Connection connection = ConnectionFactory.getConnection();
+//            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+//
+//            preparedStatement.setInt(1, targetId);
+//
+//            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+//                while (resultSet.next()) {
+//                    System.out.printf("EMPNO=%d, ENAME=%s%n", targetId, resultSet.getString("LAST_NAME"));
+//                }
+//            }
+//        } catch (SQLException e) {
+//            System.out.println("検索時にエラーが発生しました");
+//            System.out.println(e.getMessage());
+//        }
+//    }
+	
 	public static void main(String[] args) {
-        int targetId = 111;
+        String keyword = "K%";
         String sql =
-        		"SELECT LAST_NAME FROM EMPLOYEES WHERE EMPLOYEE_ID = ?";
+                "SELECT LAST_NAME FROM EMPLOYEES WHERE LAST_NAME LIKE ?";
 
         try (Connection connection = ConnectionFactory.getConnection();
-            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            preparedStatement.setInt(1, targetId);
+            preparedStatement.setString(1, keyword);
 
             try (ResultSet resultSet = preparedStatement.executeQuery()) {
                 while (resultSet.next()) {
-                    System.out.printf("EMPNO=%d, ENAME=%s%n", targetId, resultSet.getString("LAST_NAME"));
+                    System.out.printf("ENAME=%s%n", resultSet.getString("LAST_NAME"));
                 }
             }
         } catch (SQLException e) {
